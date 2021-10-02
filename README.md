150022e6577d550c3d129566c3e50b3b3187ea0d
Sankar Narayana Prasath <sankar@APPLEs-MacBook-Pro.local>
2021-10-02 17:50:43 +0530

    Text classification script

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Text-classification
+
+## Training Script
+    The traning file is inside **Train** Folder. 
+
+## Running Inference
+- Install all the required Python dependencies:
+```
+pip install -r requirements.txt
+```
+- To run inference on fast api run the follwing command:
+```
+uvicorn main:app
+```
+- The program will run in localhost. Open the browser and enter follwing address:
+```
+http://127.0.0.1:8000/docs
+```
+- Swagger will open up . We can enter text and try it out.
+
---
+++ main.py
@@ -0,0 +1,48 @@
+from typing import Optional
+
+from fastapi import FastAPI
+import tensorflow
+import nltk
+from nltk.corpus import stopwords
+stop = stopwords.words('english')
+import re
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Dense, Flatten, LSTM, Conv1D, MaxPooling1D, Dropout, Activation
+# from keras.layers.embeddings import Embedding
+import pickle
+
+## Importing tokenizer
+with open('tokenizer_final.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+
+## to clean the text --> preprocess steps
+def clean_text(list_text):
+    out = []
+    for i in list_text:
+        result = re.sub('<.*?>','',i)
+        result = re.sub('[^a-z\sA-Z]+','',result)
+        result = ' '.join([word for word in result.split() if word not in (stop)])
+        result = result.lower()
+        out.append(result)
+    return out
+
+## Load the model
+model = tensorflow.keras.models.load_model('model_text_final.hdf5',compile= False)
+
+app = FastAPI()
+
+
+
+@app.get("/text_classification/{text}")
+def read_item(text: str):
+    text_list = []
+    text_list.append(text)
+    out = clean_text(text_list) ## Clean the text
+    seq = tokenizer.texts_to_sequences(out) ## Pass text to tokenizer to get vector
+    padded = pad_sequences(seq, maxlen=50) ## padding to ensure same length of text is maintained
+    pred = model.predict(padded) ## prediction
+    print(pred)
+    test_pred = ['Positive' if num>0.5  else 'Negative' for num in pred]
+    return {"Sentiment": test_pred}
